95bb621b73fee0af45efdd1d8ad2261590181f28
Pit <orel-1993@yandex.com>
2019-06-01 13:26:12 +0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(window).on('load', function(){
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+		$('html').addClass('ios');
+	};
+	$('body').removeClass('loaded');
+});
+
+$(function(){
+
+
+	
+});
